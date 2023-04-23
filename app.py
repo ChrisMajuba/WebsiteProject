1f37457631818c428ad728bb21a422d190488ebc
ChrisMajuba <124617351+ChrisMajuba@users.noreply.github.com>
2023-04-23 22:56:53 +0000

    new files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask import render_template
-from database import get_trainers,add_applicant_db
+from database import get_experts,add_applicant_db,check_member
 #Flask is a web micro framework used to design a website using python
 
 #creating the Flask object
@@ -12,28 +12,42 @@ app = Flask("__main__")
 #***************************************************
 @app.route("/")
 def homepage():
-  data_db = get_trainers()
-  return render_template("homepage.html",data = data_db)
+  return render_template("/pages/homepage.html")
 
+@app.route("/parks_page")
+def parks():
+  return render_template("/pages/parks.html")
+
+@app.route("/experts_page")
+def experts():
+  #request infomation about the active force experts
+  data_db = get_experts()
+  return render_template("/pages/experts.html",data = data_db)
+  
 @app.route("/booking/<info>")
 def bookings_page(info):
-  return render_template("booking.html",_info=info)
+  return render_template("/pages/park_booking.html",_info=info)
 
 @app.route("/diet")
 def diet():
-  return render_template("diet.html")
+  return render_template("/content/diet.html")
 
 @app.route("/exercise/programs")
 def show_programs():
-  return render_template("programs.html")
+  return render_template("/content/programs.html")
 
 @app.route("/membership")
 def membership():
-  return render_template("membership.html")
+  return render_template("/pages/membership.html")
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+  return render_template("/pages/about.html")
+
+@app.route("/login_page")
+def login():
+  return render_template("/pages/login.html")
+
 #***************************************************
 #***************************************************
 #***************************************************
@@ -46,7 +60,7 @@ def applicant_info():
   applicant_data = dict(info)
   #send applicant to database
   add_applicant_db(applicant_data ,"session")
-  return render_template("submitted.html",data=applicant_data,string="session")
+  return render_template("/forms/submitted.html",data=applicant_data,string="session")
 
 @app.route("/membership/apply", methods=["post"])
 def membership_applicant():
@@ -54,8 +68,19 @@ def membership_applicant():
   member_applicant = dict(info)
   #send membership applicant to database
   add_applicant_db(member_applicant ,"membership")
-  return render_template("submitted.html",data=member_applicant,string="membership")
+  return render_template("/forms/submitted.html",data=member_applicant,string="membership")
 
+@app.route("/logging_in",methods = ["post"])
+def try_to_login():
+  info = request.form #request info
+  member_login = dict(info) #convert info to dictionary
+  verified_member = check_member(member_login)
+  if verified_member:
+    return jsonify(verified_member)
+  else:
+    return jsonify("Sorry you are not in the system, have you tried registering first?")
+  
+  
 #since the file main.py will be ran as a script, python will assign __name__ to __main__
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
--- database.py
+++ database.py
@@ -8,7 +8,7 @@ PASSWORD=os.getenv("password")
 DATABASE=os.getenv("database")
 
 #Function to get info from the database
-def get_trainers():
+def get_experts():
   trainers_list = []
   try:
       connection = mysql.connector.connect(host=HOST,
@@ -61,3 +61,36 @@ def add_applicant_db(applicant_dict,string):
       mycursor.close()
       connection.close()
       print("Applicant successfully sent")
+
+#Function to check if the member is registered
+def check_member(info):
+  _email,_pass = info["_email"],info["_pass"]
+  connection   = None
+  member_exist = None
+  try:
+    #connecting variable
+    connection = mysql.connector.connect(host=HOST,
+                                             database=DATABASE,
+                                             user=USERNAME,
+                                             password=PASSWORD)
+    if connection.is_connected:
+      #Check if we connected to the correct database
+      db_Info = connection.get_server_info()
+      print("Connected to MySQL Server version ", db_Info)
+      mycursor = connection.cursor()
+      #The Query to select the user infomation
+      string = "SELECT DISTINCT _name,_age,_email,_pass FROM membership WHERE _email='{}' AND _pass='{}';".format(_email,_pass)
+      print(string)
+      mycursor.execute(string)
+      member = mycursor.fetchone() #fecth the returned output
+      index  = mycursor.column_names # fecth the names of the columns
+      member_exist = dict(zip(index,member)) #create a pair of tuples using the cloumn-names and returned info
+  except Error:
+    print("error could not connect",Error)
+  finally:
+    if connection.is_connected:
+      #close the connections
+      connection.close()
+      mycursor.close()
+      print("exiting!")
+      return member_exist
